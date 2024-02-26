71fad9903ea2bc327ffb8325cc5f0e14bf2eb42f
Khushi <khushipatel3340@gmail.com>
2024-02-26 22:34:40 +0530

    Frontend Login Page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,124 @@
-import React from 'react'
+import { Container, Paper, TextField, Typography, Button } from "@mui/material";
+import React, { useState } from "react";
 
 const Login = () => {
-    return (
-        <div>Login</div>
-    )
-}
+  const [isLogin, setIsLogin] = useState(true);
 
-export default Login
+  const toggleLogin = () => {
+    setIsLogin((prev) => !prev)
+  }
+  return (
+    <Container component={"main"} maxWidth="xs" sx={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      <Paper
+        elevation={3}
+        sx={{
+          padding: 4,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        {isLogin ? (
+          <>
+            <Typography variant="h5">Login</Typography>
+            <form style={{ width: "100%", marginTop: "1rem" }}>
+              <TextField
+                required
+                fullWidth
+                label="Username"
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                margin="normal"
+                variant="outlined"
+              />
+
+              <Button
+                sx={{ marginTop: "1rem" }}
+                variant="contained"
+                color="primary"
+                type="submit"
+                fullWidth
+              >
+                Login
+              </Button>
+              <Typography textAlign={"center"} m={"1rem"}>OR</Typography>
+              <Button
+                variant="text"
+                fullWidth
+                onClick={toggleLogin}
+              >
+                Sign Up Instead
+              </Button>
+            </form>
+          </>
+        ) : (
+          <>
+            <Typography variant="h5">Login</Typography>
+            <form style={{ width: "100%", marginTop: "1rem" }}>
+              <TextField
+                required
+                fullWidth
+                label="Name"
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                required
+                fullWidth
+                label="Bio"
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                required
+                fullWidth
+                label="Username"
+                margin="normal"
+                variant="outlined"
+              />
+              <TextField
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                margin="normal"
+                variant="outlined"
+              />
+
+              <Button
+                sx={{ marginTop: "1rem" }}
+                variant="contained"
+                color="primary"
+                type="submit"
+                fullWidth
+              >
+                Sign Up
+              </Button>
+              <Typography textAlign={"center"} m={"1rem"}>OR</Typography>
+              <Button
+                variant="text"
+                fullWidth
+                onClick={toggleLogin}
+              >
+                Login Instead
+              </Button>
+            </form>
+          </>
+        )}
+      </Paper>
+    </Container>
+  );
+};
+
+export default Login;
